94e29d4ca738c5c50d62234e3443738de4ff7d94
huchenghao <578849310@qq.com>
2017-09-20 19:19:14 +0800

    DateUtils

---
+++ src/main/java/com/ht/DateHelper.java
@@ -0,0 +1,115 @@
+package com.ht;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * 
+ * @ClassName: DateHelper
+ * @Description: 日期工具类
+ * @author: huchenghao
+ * @date: 2017年9月20日 下午6:57:23
+ */
+public class DateHelper {
+	private static final String YEAR_MONTH_DAY_TEMPLATE = "yyyy-MM-dd";
+	private static final String YEAR_MONTH_DAY_HOUR_MINUTE_SECOND_TEMPLATE = "yyyy-MM-dd hh:mm:ss";
+	
+	private static SimpleDateFormat df_ymd=new SimpleDateFormat(YEAR_MONTH_DAY_TEMPLATE);
+	private static SimpleDateFormat df_ymdhms=new SimpleDateFormat(YEAR_MONTH_DAY_HOUR_MINUTE_SECOND_TEMPLATE);
+	
+	/**
+	 * 
+	 * @Title: longTimeForYMDHMSStr
+	 * @Description: long类型的时间戳转换为yyyy-MM-dd hh:mm:ss格式的字符串
+	 * @param currentTimeMillis
+	 * @return
+	 * @return: String
+	 */
+	public static String longTimeForYMDHMSStr(Long currentTimeMillis){
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(currentTimeMillis);
+		return  df_ymdhms.format(calendar.getTime());
+	}
+	/**
+	 * 
+	 * @Title: formatDateToYMDHMSStr
+	 * @Description: Date格式日期转换为yyyy-MM-dd hh:mm:ss格式的字符串
+	 * @param date
+	 * @return
+	 * @return: String
+	 */
+	public static String formatDateForYMDHMSStr(Date date){
+		return df_ymdhms.format(date);
+	}
+	/**
+	 * 
+	 * @Title: formatDateToYMDStr
+	 * @Description: Date格式日期转换为yyyy-MM-dd格式的字符串
+	 * @param date
+	 * @return
+	 * @return: String
+	 */
+	public static String formatDateToYMDStr(Date date){
+		return df_ymd.format(date);
+	}
+	/**
+	 * 
+	 * @Title: formatDate
+	 * @Description: Date格式日期转换为format格式的字符串
+	 * @param date
+	 * @param format
+	 * @return
+	 * @return: String
+	 */
+	public static String formatDate(Date date,String format){
+		SimpleDateFormat df = new SimpleDateFormat(format);
+		return df.format(date);
+	}
+	/**
+	 * 
+	 * @Title: getTodayStr
+	 * @Description: 获取String格式的，今天的日期
+	 * @return
+	 * @return: String
+	 */
+	public static String getTodayStr(){
+		return df_ymd.format(new Date());
+	}
+	/**
+	 * 
+	 * @Title: comparDate
+	 * @Description: 比较两个Date的大小，1>2:1;1<2:0;1=null || 2== null:-1
+	 * @param date1
+	 * @param date2
+	 * @return
+	 * @return: int
+	 */
+	public static int comparDate(Date date1,Date date2){
+		if(null == date1 || null == date2){
+			return -1;
+		}else if(date1.getTime() > date2.getTime()){
+			return 1;
+		}else{
+			return 0;
+		}
+	}
+	/**
+	 * 
+	 * @Title: parseYMDStrForDate
+	 * @Description: TODO
+	 * @param dateStr
+	 * @return
+	 * @throws ParseException
+	 * @return: Date
+	 */
+	public static Date parseYMDStrForDate(String dateStr) throws ParseException{
+		return df_ymd.parse(dateStr);
+	}
+	
+	
+	
+	
+}
