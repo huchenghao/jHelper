807a69ce14913d578065613669ba12b174c48483
huchenghao <578849310@qq.com>
2018-07-10 19:54:50 +0800

    BooleanUtil代码整理，注释详细

--- src/main/java/com/ht/base/BooleanUtil.java
+++ src/main/java/com/ht/base/BooleanUtil.java
@@ -1,73 +1,138 @@
 package com.ht.base;
 import org.apache.commons.lang3.BooleanUtils;
+
 /**
- * 1. 从String(true/false, yes/no)，转换为Boolean或boolean
  * 
- * 2. 逻辑运算：取反，多个boolean的and,or 计算
- * 
- * 封装 {@code org.apache.commons.lang3.BooleanUtils}
+ * @ClassName: BooleanUtil
+ * @Description: 
+ * 				1. 从String(true/false, yes/no)，转换为Boolean或boolean
+ * 				2. 逻辑运算：取反，多个boolean的and,or 计算
+ * @author: huchenghao
+ * @date: 2018年7月10日 下午6:39:27
  */
 public class BooleanUtil {
 	/**
-	 * 使用标准JDK，只分析是否忽略大小写的"true", str为空时返回false
+	 * @Title: toBoolean
+	 * @Description: str="true"-->true;<br/>
+	 * 				 str="TRUE"-->true;<br/>
+	 * 				 str=""-->false;<br/>
+	 * 				 str="其它字符串"-->false<br/>
+	 * @param str
+	 * @return boolean
+	 * @author huchenghao
 	 */
 	public static boolean toBoolean(String str) {
 		return Boolean.parseBoolean(str);
 	}
-
 	/**
-	 * 使用标准JDK，只分析是否忽略大小写的"true", str为空时返回null
+	 * @Title: toBooleanObject
+	 * @Description: str="true"-->true;<br/>
+	 * 				 str="TRUE"-->true;<br/>
+	 * 				 str=""-->false;<br/>
+	 * 				 str="其它字符串"-->false<br/>
+	 * 				 str=null-->null<br/>
+	 * @param str
+	 * @return Boolean
+	 * @author huchenghao
 	 */
 	public static Boolean toBooleanObject(String str) {
 		return str != null ? Boolean.valueOf(str) : null;
 	}
 
 	/**
-	 * 使用标准JDK，只分析是否忽略大小写的"true", str为空时返回defaultValue
+	 * 
+	 * @Title: toBooleanObject
+	 * @Description: 只分析是否忽略大小写的"true"<br/>
+	 * 				 str为空时返回defaultValue
+	 * @param str
+	 * @param defaultValue
+	 * @return Boolean
+	 * @author huchenghao
 	 */
 	public static Boolean toBooleanObject(String str, Boolean defaultValue) {
 		return str != null ? Boolean.valueOf(str) : defaultValue;
 	}
 
 	/**
-	 * 支持true/false, on/off, y/n, yes/no的转换, str为空或无法分析时返回null
+	 * 
+	 * @Title: parseGeneralString
+	 * @Description: 支持true/false, on/off, y/n, yes/no的转换<br/>
+	 * 				 <b>str为空或无法分析时返回null</b>
+	 * @param str
+	 * @return
+	 * @author huchenghao
 	 */
 	public static Boolean parseGeneralString(String str) {
 		return BooleanUtils.toBooleanObject(str);
 	}
 
 	/**
-	 * 支持true/false,on/off, y/n, yes/no的转换, str为空或无法分析时返回defaultValue
+	 * 
+	 * @Title: parseGeneralString
+	 * @Description: 支持true/false,on/off, y/n, yes/no的转换<br/>
+	 * 				 <b>str为空或无法分析时返回defaultValue</b>
+	 * @param str
+	 * @param defaultValue
+	 * @return
+	 * @author huchenghao
 	 */
 	public static Boolean parseGeneralString(String str, Boolean defaultValue) {
 		return BooleanUtils.toBooleanDefaultIfNull(BooleanUtils.toBooleanObject(str), defaultValue);
 	}
 
+	
 	/**
-	 * 取反
+	 * 
+	 * @Title: negate
+	 * @Description: 取反
+	 * @param bool
+	 * @return
+	 * @author huchenghao
 	 */
 	public static boolean negate(final boolean bool) {
 		return !bool;
 	}
-
 	/**
-	 * 取反
+	 * 
+	 * @Title: negate
+	 * @Description: 取反
+	 * @param bool
+	 * @return
+	 * @author huchenghao
 	 */
 	public static Boolean negate(final Boolean bool) {
 		return BooleanUtils.negate(bool);
 	}
 
 	/**
-	 * 多个值的and
+	 * 
+	 * @Title: and
+	 * @Description: 多个值的and<br/>
+	 * 				 true,true-->true<br/>
+	 *  			 false,true-->false<br/>
+	 *  			 false,false-->false<br/>
+	 * @param array
+	 * @return
+	 * @author huchenghao
 	 */
 	public static boolean and(final boolean... array) {
 		return BooleanUtils.and(array);
 	}
 
 	/**
-	 * 多个值的or
+	 * 
+	 * @Title: or
+	 * @Description: 多个值的or<br/>
+	 * 				 true,true-->true<br/>
+	 *  			 false,true-->true<br/>
+	 *  			 false,false-->false<br/>
+	 * @param array
+	 * @return
+	 * @author huchenghao
 	 */
 	public static boolean or(final boolean... array) {
 		return BooleanUtils.or(array);
 	}
+	
+	
 }
