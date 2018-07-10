977aa4166399aeaccfa65e1a77e92d8f52a34847
huchenghao <578849310@qq.com>
2018-07-10 18:37:18 +0800

    初始化jHelper

---
+++ src/main/java/com/ht/common/MessageCode.java
@@ -0,0 +1,24 @@
+package com.ht.common;
+/**
+ * MessageInfo 返回code 表述
+ * */
+public class MessageCode {
+	/**
+	 * 成功 code
+	 * */
+	public static  final String SUCCESS ="0";
+	
+	/**
+	 * 失败(主要针对代码异常) code
+	 * */
+	public static  final String ERROR = "1";
+	/**
+	 * tips
+	 */
+	public static  final String TIPS = "2";
+	/**
+	 * 签名认证失败
+	 */
+	public static final String UNAUTHORIZED="3";
+	
+}
