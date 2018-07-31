0b9a41b62d9fb516d691c46423f1dbce98c4a5af
huchenghao <578849310@qq.com>
2018-07-31 12:10:18 +0800

    标注NOTNULL NUALLABLE

--- src/main/java/com/ht/annotation/NotNull.java
+++ src/main/java/com/ht/annotation/NotNull.java
@@ -4,7 +4,11 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 /**
- * 标注参数、属性、方法不可为 Null
+ * 
+ * @ClassName: NotNull
+ * @Description: 标注参数、属性、方法不可为 Null
+ * @author: huchenghao
+ * @date: 2018年7月31日 下午12:09:20
  */
 @Target({ElementType.PARAMETER, ElementType.FIELD, ElementType.METHOD})
 public @interface NotNull {
