05286ea3212cf928205f3b7933b8ae3db27d2b3d
user.email <524245834@qq.com>
2018-08-15 17:35:03 +0800

    测试111

--- src/main/java/com/ht/aa/HtUtil.java
+++ src/main/java/com/ht/aa/HtUtil.java
@@ -5,6 +5,7 @@ public class HtUtil {
 	public static String getStr(){
 		System.out.println("666666666");
 		System.out.println("777777777");
+		System.out.println("888888888");
 		return "aa";
 	}
 }
