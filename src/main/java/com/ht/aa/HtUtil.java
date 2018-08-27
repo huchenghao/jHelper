013dec805f85c4af72f1124cc3925bc1aa4cdb79
952051088@qq.com <952051088@qq.com>
2018-08-27 17:10:35 +0800

    测试删除main方法

--- src/main/java/com/ht/aa/HtUtil.java
+++ src/main/java/com/ht/aa/HtUtil.java
@@ -2,13 +2,4 @@ package com.ht.aa;
 
 public class HtUtil {
 
-	public static void main(String[] args) {
-	
-		System.out.println("666666666");
-		System.out.println("777777777");
-		System.out.println("888888888");
-		System.out.println("999999999");
-		System.out.println("111111111");
-		return "aa";
-	}
 }
