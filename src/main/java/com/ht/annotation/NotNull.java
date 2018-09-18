63900dd0f39da104e89454fc1b6af84469d5026c
huchenghao <578849310@qq.com>
2018-09-18 18:13:39 +0800

    新增防重复提交注解

--- src/main/java/com/ht/annotation/NotNull.java
+++ src/main/java/com/ht/annotation/NotNull.java
@@ -1,6 +1,8 @@
 package com.ht.annotation;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
