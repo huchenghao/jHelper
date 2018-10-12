3223b8cb2ba957229b010f35b0c1fe4a62e86d3b
huchenghao <578849310@qq.com>
2018-10-12 17:03:29 +0800

    修复DateUtil

--- src/main/java/com/ht/time/DateFormatUtil.java
+++ src/main/java/com/ht/time/DateFormatUtil.java
@@ -145,19 +145,19 @@ public class DateFormatUtil {
 			// 'c' 日期和时间，被格式化为 "%ta %tb %td %tT %tZ %tY"，例如 "Sun Jul 20 16:17:00 EDT 1969"。
 			return String.format("%tc", timeStampMillis);
 		}
-		if (span < DateUtil.MILLIS_PER_SECOND) {
+		if (span < DateUtil2.MILLIS_PER_SECOND) {
 			return "刚刚";
-		} else if (span < DateUtil.MILLIS_PER_MINUTE) {
-			return String.format("%d秒前", span / DateUtil.MILLIS_PER_SECOND);
-		} else if (span < DateUtil.MILLIS_PER_HOUR) {
-			return String.format("%d分钟前", span / DateUtil.MILLIS_PER_MINUTE);
+		} else if (span < DateUtil2.MILLIS_PER_MINUTE) {
+			return String.format("%d秒前", span / DateUtil2.MILLIS_PER_SECOND);
+		} else if (span < DateUtil2.MILLIS_PER_HOUR) {
+			return String.format("%d分钟前", span / DateUtil2.MILLIS_PER_MINUTE);
 		}
 		// 获取当天00:00
-		long wee = DateUtil.beginOfDate(new Date(now)).getTime();
+		long wee = DateUtil2.beginOfDate(new Date(now)).getTime();
 		if (timeStampMillis >= wee) {
 			// 'R' 24 小时制的时间，被格式化为 "%tH:%tM"
 			return String.format("今天%tR", timeStampMillis);
-		} else if (timeStampMillis >= wee - DateUtil.MILLIS_PER_DAY) {
+		} else if (timeStampMillis >= wee - DateUtil2.MILLIS_PER_DAY) {
 			return String.format("昨天%tR", timeStampMillis);
 		} else {
 			// 'F' ISO 8601 格式的完整日期，被格式化为 "%tY-%tm-%td"。
