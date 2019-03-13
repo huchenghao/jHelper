6138a2890ebb6627e1bf051fe5cf170998696eb3
guo1229 <524245834@qq.com>
2019-03-13 10:03:38 +0800

    wsdrthfghn

--- src/main/java/com/ht/time/ClockUtil.java
+++ src/main/java/com/ht/time/ClockUtil.java
@@ -6,7 +6,6 @@ import java.util.Date;
  * 
  * 平时使用DEFAULT，测试时替换为DummyClock，可准确控制时间变化而不用Thread.sleep()等待时间流逝.
  */
-
 public class ClockUtil {
 
 	private static Clock instance = new DefaultClock();
