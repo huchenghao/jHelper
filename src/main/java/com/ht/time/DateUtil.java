fc21542f36113c40e1b36c5bcddc79257cd1b52d
52424 <52424@gs>
2019-04-15 19:02:53 +0800

    增加日期方法

--- src/main/java/com/ht/time/DateUtil.java
+++ src/main/java/com/ht/time/DateUtil.java
@@ -924,12 +924,28 @@ public class DateUtil {
 		return retList;
 	}
 	
-	public static void main(String[] args) throws Exception, Exception {
-		
-//		double timeInterval = getTimeIntervalHM(parseYMDHMSStrToDate("2018-01-01 12:00:00"), parseYMDHMSStrToDate("2018-01-02 14:00:00")
-//				,parseYMDHMSStrToDate("2018-01-01 11:00:00"), parseYMDHMSStrToDate("2018-01-02 15:00:00"));
-		List<Map<String,Object>> list = getTimeQuantumArray("2018-01-01 12:00:00", "2018-01-05:00:00");
-		System.out.println(list);
-		
+	/**
+	 * 获取本周的周一时间
+	 * Description: 
+	 * @author gs
+	 * @date 2019年4月15日下午6:47:12 
+	 * @tags @param date
+	 * @tags @return
+	 */
+	public static Date getThisWeekMonday(Date date) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		// 获得当前日期是一个星期的第几天
+		int dayWeek = cal.get(Calendar.DAY_OF_WEEK);
+		if (1 == dayWeek) {
+			cal.add(Calendar.DAY_OF_MONTH, -1);
+		}
+		// 设置一个星期的第一天，按中国的习惯一个星期的第一天是星期一
+		cal.setFirstDayOfWeek(Calendar.MONDAY);
+		// 获得当前日期是一个星期的第几天
+		int day = cal.get(Calendar.DAY_OF_WEEK);
+		// 根据日历的规则，给当前日期减去星期几与一个星期第一天的差值
+		cal.add(Calendar.DATE, cal.getFirstDayOfWeek() - day);
+		return cal.getTime();
 	}
 }
